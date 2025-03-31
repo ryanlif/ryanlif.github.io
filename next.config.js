c4364fe9d79411f4b6d1ade5fd425095a696bbeb
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-03-31 11:05:43 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withOffline = require('next-offline');
 const nextConfig = {
 	poweredByHeader: false,
 	swcMinify: true,
+	output: "export",  // <=== enables static exports
+  	reactStrictMode: true,
 };
 
 module.exports = withOffline(nextConfig);
