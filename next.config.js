723e2de3be19aeba885a34b10a7842e036515e3f
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-03-31 11:19:18 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withOffline = require('next-offline');
 
 const nextConfig = {
+	output: "export",  // <=== enables static exports
+	basePath: "/ryanlif.github.io",
 	poweredByHeader: false,
 	swcMinify: true,
-	output: "export",  // <=== enables static exports
   	reactStrictMode: true,
+	images: {
+    unoptimized: true,},
 };
 
 module.exports = withOffline(nextConfig);
