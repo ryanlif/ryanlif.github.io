ac14a5b3413f369a9b58f73a3832fb216a494101
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-04-01 14:08:51 -0700

    Update and rename Teaching.tsx to TeachingItem.tsx

---
+++ components/TeachingItem.tsx
@@ -0,0 +1,26 @@
+import ExtLink from './ExtLink';
+
+interface Props {
+    teaching: any;
+    index: number;
+}
+
+const TeachingItem = ({teaching, index}: Props): JSX.Element => {
+    return (
+        <div className = "mt-4 mb-8">
+            <p className = "text-base text-gray-500">
+                [{index}] {teaching.course} <b><i>{teaching.responsibility}</i></b><br/>
+            </p>
+            <p className = "text-sm">{teaching.description}</p>
+            <p className = "text-gray-500 flex justify-end text-sm bold">
+                {teaching.links.map((linkItem: any, idx: any) => (
+                    <ExtLink href={linkItem.url} key={idx}> [{linkItem.name}] &nbsp;</ExtLink>
+                ))}                
+            </p>
+            
+        </div>
+
+    );
+};
+
+export default TeachingItem;
