8d21e3b3760e5d123d4e580f6645acdb9bdba706
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-04-01 13:52:19 -0700

    Update teaching.tsx

--- pages/teaching.tsx
+++ pages/teaching.tsx
@@ -1,14 +1,14 @@
 import {NextPage} from 'next';
 
 import TeachingList from '../components/TeachingList';
-import LP from '../components/INDE310';
-import IPDP from '../components/INDE412';
+import INDE310 from '../components/INDE310';
+import INDE412 from '../components/INDE412';
 
 const Teaching: NextPage<unknown> = () => (
     <>
         <TeachingList/>
-        <LP/>
-        <IPDP/>
+        <INDE310/>
+        <INDE412/>
     </>
 );
 
