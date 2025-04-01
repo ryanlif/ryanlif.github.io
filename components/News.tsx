670d7f3418bf1f1ea3022c88002131ede4bc7bf8
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-04-01 16:37:14 -0700

    Update News.tsx

--- components/News.tsx
+++ components/News.tsx
@@ -1,1 +1,21 @@
+import data from './data/news.json'
 
+const News = (): JSX.Element => {
+    return (
+        <section className="grid" id="news">
+            <h2 className="text-xl font-bold mt-12 mb-4">News</h2>
+            <div className="text-base ml-4 text-gray-700">
+                <ul className="list-disc">
+                  {data.map((updateInfo) => (
+                     <li key={updateInfo.date}>TA: {updateInfo.id}, {updateInfo.name}</li>
+                  ))}
+                </ul>
+            </div>
+
+        </section>
+    );
+};
+
+
+
+export default Teaching;
