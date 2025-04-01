31efa79498d8b1883c6a29293f9319e069393aee
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-04-01 14:02:59 -0700

    Update TeachingList.tsx

--- components/TeachingList.tsx
+++ components/TeachingList.tsx
@@ -1,1 +1,20 @@
+import TeachingItem from './PublicationItem'
+import data from './data/teaching.json'
 
+const TeachingList = (): JSX.Element => {
+    return (
+        <section className="grid w-full" id="teaching">
+            <h2 className="text-xl font-bold mt-12 mb-4">Teaching</h2>
+            <div>
+              {data.map((teaching, index) => (
+                <TeachingItem teaching={teaching} index={index} key={index}/>
+              ))}
+            </div>
+
+        </section>
+    );
+};
+
+
+
+export default TeachingList;
