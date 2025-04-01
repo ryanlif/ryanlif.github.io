a7fc5aebc9b0aeb47329e0e2f3ca335acd5fed41
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-04-01 14:11:12 -0700

    Update teaching.tsx

--- pages/teaching.tsx
+++ pages/teaching.tsx
@@ -1,15 +1,9 @@
 import {NextPage} from 'next';
 
 import TeachingList from '../components/TeachingList';
-import TeachingItem from '../components/TeachingItem';
-import Inde412 from '../components/Inde412';
 
 const Teaching: NextPage<unknown> = () => (
-    <>
-        <TeachingList/>
-        <Inde310/>
-        <Inde412/>
-    </>
+    <TeachingList/>
 );
 
 export default Teaching;
