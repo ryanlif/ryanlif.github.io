11e59ba5f6d3b57570a96c436c0a350093c055f6
Ryan <85737434+ryanlif@users.noreply.github.com>
2025-04-01 09:46:39 -0700

    Update ProfileImage.tsx

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -5,7 +5,7 @@ const ProfileImage = (): JSX.Element => {
     return (
         <Image
             className="rounded-full select-none transition-all pointer-events-none"
-            src="/images/me.jpg"
+            src="/images/myself.jpg"
             // onMouseOver = {e => e.currentTarget.src = "/images/gelo.HEIC"}
             // onMouseOut = {e => e.currentTarget.src = "/images/me.jpg"}
             draggable={false}
