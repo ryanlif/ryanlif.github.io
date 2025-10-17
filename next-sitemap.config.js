7dd9d904f5b6d3e29d69e0faae383449f0f5a4bc
ryanlif <ryanflin@uw.edu>
2025-10-17 15:51:56 -0700

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: process.env.SITE_URL || 'https://ryanflin.com',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 }
