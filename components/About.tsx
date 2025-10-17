e29895bf8a1e86f2000b223555a21a71589ceaa9
ryanlif <ryanflin@uw.edu>
2025-10-17 15:44:34 -0700

    dark mode colors

--- components/About.tsx
+++ components/About.tsx
@@ -20,7 +20,7 @@ const About = (): JSX.Element => {
 						advised by Dr. <ExtLink href={personalInfo.about.advisor.link}> {personalInfo.about.advisor.name}. </ExtLink>
 						 <br/><a className = "text-sm">✉️ {personalInfo.about.email}</a>
 					</p>
-					<p className = "text-sm">
+					<p className = "text-sm text-gray-600">
 						Research Interest: {personalInfo.about.interest}
 					</p>
 					<p className = "text-sm text-gray-400">
--- components/News.tsx
+++ components/News.tsx
@@ -4,7 +4,7 @@ const News = (): JSX.Element => {
     return (
         <section className="grid" id="news">
             <h2 className="text-xl font-bold mt-12 mb-4">News</h2>
-            <div className="text-base ml-4 text-gray-700">
+            <div className="text-base ml-4 text-gray-700 dark:text-white">
                 <ul className="list-disc">
                   {data.map((updateInfo) => (
                      <li> <b>{updateInfo.date}:</b> {updateInfo.description}</li>
