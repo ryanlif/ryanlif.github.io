b796f80ff5407da61614ce4df4ccde1998d78624
ryanlif <ryanflin@uw.edu>
2025-10-17 23:17:47 -0700

    teaching updates

--- components/TeachingItem.tsx
+++ components/TeachingItem.tsx
@@ -8,12 +8,12 @@ interface Props {
 const TeachingItem = ({teaching, index}: Props): JSX.Element => {
     return (
         <div className = "mt-4 mb-8">
-            <p className = "text-base text-gray-700 dark: text-gray-100">
+            <p className = "text-base text-gray-700 dark:text-gray-100">
                 [{index}] {teaching.course} &nbsp;&nbsp;&nbsp;<b>{teaching.responsibility}</b><br/>
             </p>
-            <p className = "text-base text-gray-700 dark: text-gray-200"><i>{teaching.period}</i></p>
+            <p className = "text-base text-gray-700 dark:text-gray-200"><i>{teaching.period}</i></p>
             <p className = "text-base">{teaching.description}</p>
-            <p className = "text-base text-gray-700 dark: text-gray-300 flex justify-end text-sm bold">
+            <p className = "text-base text-gray-700 dark:text-gray-300 flex justify-end bold">
                 {teaching.links.map((linkItem: any, idx: any) => (
                     <ExtLink href={linkItem.url} key={idx}> [{linkItem.name}] &nbsp;</ExtLink>
                 ))}                
