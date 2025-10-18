e47cee5f0d512548425567409108bc638ccde01f
ryanlif <ryanflin@uw.edu>
2025-10-18 13:31:05 -0700

    pub update

--- components/PublicationItem.tsx
+++ components/PublicationItem.tsx
@@ -28,11 +28,11 @@ const PublicationItem = ({publication, index}: Props): JSX.Element => {
     return (
         <div className = "mt-4 mb-8">
             <p className = "text-base text-gray-600 dark:text-gray-100">
-                [{index}] <span dangerouslySetInnerHTML={{ __html: publication.author }}/>{', '} <b><i>{publication.title}</i></b><br/>
-                <a className = "text-sm">{publication.conference}</a>{', '}<a className="text-sm">{publication.year}</a>
-                <p className = "text-sm bold ">{publication.note}</p>
+                [{index}] <span dangerouslySetInnerHTML={{ __html: publication.author }}/>{', '} <b>{publication.title}</b><br/>
+                <a className = "text-base"><i>{publication.conference}</i></a>{', '}<a className="text-sm">{',' && publication.year}</a>
             </p>
-            <p className = "text-gray-600 dark:text-gray-200 flex justify-end text-sm bold">
+            <p className = "text-base text-gray-600 dark:text-gray-200 flex justify-end text-sm bold">
+                <span className = "text-base bold ">{publication.note}</span>
                 {publication.links.map((linkItem: any, idx: any) => (
                     <ExtLink href={linkItem.url} key={idx}> [{linkItem.name}] &nbsp;</ExtLink>
                 ))}                
