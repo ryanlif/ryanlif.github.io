7e9ce3d9e2966f09a35f2a44d190d165e71a0015
ryanlif <ryanflin@uw.edu>
2025-10-17 20:54:05 -0700

    teaching update full

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -1,20 +1,22 @@
 import ExtLink from './ExtLink';
 
-interface ProjectProps {
+interface Props {
     project: any;
     index: number;
 }
 
-const ProjectItem = ({project, index}: ProjectProps): JSX.Element => {
+const ProjectItem = ({project, index}: Props): JSX.Element => {
     return (
       <div className="shadow-lg mb-8 mx-auto lg:w-11/12 lg:flex lg:flex-row lg:h-auto">
         <img className="rounded-tr-lg rounded-tl-lg h-68 w-full lg:h-auto lg:w-6/12 lg:rounded-bl-lg lg:rounded-tr-none" src={project.img} alt="" />
         <div className="w-full bg-gray-50 p-8 rounded-bl-lg rounded-br-lg lg:rounded-bl-none lg:rounded-tr-lg">
           <h2 className="text-gray-700 font-semibold">{project.title}</h2>
           <p className="text-sm text-gray-500 mt-4">{project.description}</p>
-          {project.links.map((linkItem: any, idx: any) => (
+          <p className = "text-gray-700 flex justify-end text-sm bold">
+                {project.links.map((linkItem: any, idx: any) => (
                     <ExtLink href={linkItem.url} key={idx}> [{linkItem.name}] &nbsp;</ExtLink>
                 ))} 
+          </p>
         </div>
       </div>
     )
