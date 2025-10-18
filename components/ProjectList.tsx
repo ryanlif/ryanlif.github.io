4f3abaa16a10593676501681d30ce64234cd1b05
ryanlif <ryanflin@uw.edu>
2025-10-18 09:18:49 -0700

    publications

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -3,7 +3,7 @@ import data from './data/projects.json'
 
 const ProjectList = (): JSX.Element => {
     return (
-        <section className="grid" id="publications">
+        <section className="grid" id="projects">
             <h2 className="text-xl font-bold mt-12 mb-4">Projects</h2>
             <div>
                 {data.map((project, index) => (
--- components/PublicationList.tsx
+++ components/PublicationList.tsx
@@ -3,8 +3,9 @@ import data from './data/publications.json'
 
 const PublicationList = (): JSX.Element => {
     return (
-        <section className="grid w-full" id="publications">
+        <section className="grid w-full" id="publication">
             <h2 className="text-xl font-bold mt-12 mb-4">Publications</h2>
+            <embed src="https://drive.google.com/drive/u/0/home" width="500" height="375" />
             <div>
               {data.map((publication, index) => (
                 <PublicationItem publication={publication} index={index} key={index}/>
