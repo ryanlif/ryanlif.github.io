783c1bf58ac422cbab3e8b45698d27c8e0eac415
ryanlif <ryanflin@uw.edu>
2025-10-18 12:23:32 -0700

    add instructions correcte

--- components/PublicationItem.tsx
+++ components/PublicationItem.tsx
@@ -28,7 +28,7 @@ const PublicationItem = ({publication, index}: Props): JSX.Element => {
     return (
         <div className = "mt-4 mb-8">
             <p className = "text-base text-gray-500">
-                [{index}] {publication.author}{' '} <b><i>{publication.title}</i></b><br/>
+                [{index}] <span dangerouslySetInnerHTML={{ __html: publication.author }}/>{' '} <b><i>{publication.title}</i></b><br/>
                 <a className = "text-sm">{publication.conference}</a>
             </p>
             <p className = "text-gray-500 flex justify-end text-sm bold">
