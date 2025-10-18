4bed99822efac071f5e01b286c741ab6b59a7ee9
ryanlif <ryanflin@uw.edu>
2025-10-18 08:53:18 -0700

    jump to publications

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,7 @@ const Header = ({mounted, resolvedTheme, setTheme, scrolled}: HeaderProps): JSX.
                 </Link>
                 <nav className="flex items-center justify-between pr-4">
                     <NavLink title="About" href="/#about"/>
-                    <NavLink title="Publications" href="/#publications"/>
+                    <NavLink title="Publications" href="/publications"/>
                     <NavLink title="Projects" href="/projects"/>
                     <NavLink title="Teaching" href="/teaching"/>
                     <NavLink title="Service" href="/misc"/>
