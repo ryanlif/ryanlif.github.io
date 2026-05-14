0f3b05e1baf91e19514c4c1f44c06d5ac0bca64c
Ryan Lin <ryanlif@Ryans-MacBook-Pro.local>
2026-05-13 22:00:12 -0700

    update fold

--- components/TeachingList.tsx
+++ components/TeachingList.tsx
@@ -21,7 +21,24 @@ const TeachingList = (): JSX.Element => {
               ))}
             </div> */}
             <h2 className="text-xl font-bold mt-12 mb-4">Courses Taught at University of Washington</h2>
-            <div> 
+            <div>
+            {data.slice(0, 1).map((teaching, index) => (
+              <TeachingItem teaching={teaching} index={index} key={index}/>
+            ))}
+
+            <details className="my-6">
+            <summary className="cursor-pointer text-lg font-medium text-gray-400 dark:text-gray-600">
+              Past Courses
+            </summary>
+
+            <div className="mt-4">
+              {data.slice(1).map((teaching, index) => (
+                <TeachingItem teaching={teaching} index={index + 1} key={index}/>
+              ))}
+            </div>
+            </details>
+            </div>
+            {/* <div> 
               {data.map((teaching, index) => (
                 <div key={index}>
                     <TeachingItem teaching={teaching} index={index} key={index}/>
@@ -36,7 +53,7 @@ const TeachingList = (): JSX.Element => {
                     )}
                 </div>
               ))}
-            </div>
+            </div> */}
 
         </section>
     );
