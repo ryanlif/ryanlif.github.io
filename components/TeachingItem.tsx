a76dfe713a20734b068d95febd97af9700a2a67e
Ryan <85737434+ryanlif@users.noreply.github.com>
2026-05-13 21:22:02 -0700

    Update TeachingItem.tsx

--- components/TeachingItem.tsx
+++ components/TeachingItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 const TeachingItem = ({teaching, index}: Props): JSX.Element => {
     const mainTextClass = teaching.active
         ? 'text-black dark:text-white'
-        : 'text-gray-300 dark:text-gray-700';
+        : 'text-gray-500 dark:text-gray-700';
 
     const subTextClass = teaching.active
         ? 'text-gray-900 dark:text-gray-50'
