759ac53d128d9a83e2697b73c0d39d2aaa5a2bea
Ryan <85737434+ryanlif@users.noreply.github.com>
2026-05-13 21:17:01 -0700

    Update TeachingItemMSU.tsx

--- components/TeachingItemMSU.tsx
+++ components/TeachingItemMSU.tsx
@@ -8,18 +8,18 @@ interface Props {
 const TeachingItemMSU = ({teaching, index}: Props): JSX.Element => {
     const mainTextClass = teaching.active
         ? 'text-black dark:text-white'
-        : 'text-gray-500 dark:text-gray-500';
+        : 'text-gray-300 dark:text-gray-700';
 
     const subTextClass = teaching.active
         ? 'text-gray-900 dark:text-gray-50'
-        : 'text-gray-300 dark:text-gray-700';
+        : 'text-gray-100 dark:text-gray-900';
 
     return (
         <div className = "mt-4 mb-8">
             <p className={`text-base ${mainTextClass}`}>
                 [{index}] {teaching.course} &nbsp;&nbsp;&nbsp;<b>{teaching.responsibility}</b><br/>
             </p>
-            <p className={`text-base ${subTextClass}`}><i>{teaching.period}</i></p>
+            <p className={`text-base ${mainTextClass}`}><i>{teaching.period}</i></p>
             <p className={`text-base ${subTextClass}`}>{teaching.description}</p>
             <p className={`text-base ${subTextClass} flex justify-end bold`}>
                 {teaching.links.map((linkItem: any, idx: any) => (
