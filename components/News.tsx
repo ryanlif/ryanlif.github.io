9e4bc25efee357c03d7934e7f7da3d29a3d351a1
Ryan Lin <ryanlif@Ryans-MacBook-Pro.local>
2026-05-13 22:07:47 -0700

    add old news fold

--- components/News.tsx
+++ components/News.tsx
@@ -6,11 +6,34 @@ const News = (): JSX.Element => {
         <section className="grid" id="news">
             <h2 className="text-xl font-bold mt-12 mb-4">News</h2>
             <div className="text-base ml-4 text-gray-700 dark:text-gray-300">
-                <ul className="list-disc">
+                {/* <ul className="list-disc">
                   {data.map((updateInfo) => (
                      <li> <span className="dark:text-white"><b>{updateInfo.date}:</b></span> {updateInfo.description}</li>
                   ))}
+                </ul> */}
+                <ul className="list-disc">
+                    {data.slice(0, 10).map((updateInfo, index) => (
+                        <li key={index}>
+                        <span className="dark:text-white"><b>{updateInfo.date}:</b></span> {updateInfo.description}
+                        </li>
+                    ))}
                 </ul>
+
+                {data.length > 10 && (
+                    <details className="mt-4">
+                    <summary className="cursor-pointer text-base font-medium text-gray-500 dark:text-gray-400">
+                            Old News
+                    </summary>
+
+                    <ul className="list-disc mt-3">
+                    {data.slice(10).map((updateInfo, index) => (
+                        <li key={index}>
+                        <span className="dark:text-white"><b>{updateInfo.date}:</b></span> {updateInfo.description}
+                        </li>
+                    ))}
+                    </ul>
+                    </details>
+                )} 
             </div>
 
         </section>
